cfe2db0fe343f0ef75051876c967a44f90fdbe6e
keshitrivedi <keshitrivedi@gmail.com>
2025-04-06 02:02:14 +0530

    Add new images to Gallery, fix pagination

--- components/Home/Gallery.jsx
+++ components/Home/Gallery.jsx
@@ -20,6 +20,8 @@ import img8 from "../../public/Home/8.JPG";
 import img9 from "../../public/Home/9.JPG";
 import img10 from "../../public/Home/10.JPG";
 import img11 from "../../public/Home/11.jpg";
+import img12 from "../../public/Home/12.JPG";
+import img13 from "../../public/Home/13.JPG";
 
 const serviceData = [
     img1,
@@ -32,7 +34,9 @@ const serviceData = [
     img8,
     img9,
     img10,
-    img11
+    img11,
+    img12,
+    img13
 ];
 
 const ServiceSlider = () => {
@@ -83,7 +87,7 @@ const ServiceSlider = () => {
             //     slideShadows: false
             // }}
             pagination={{
-                dynamicBullets: true
+                clickable: true
             }}
             autoplay={{
                 delay: 2000,
